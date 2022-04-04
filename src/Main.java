c0d1c0df34c8bdc119c519f1075cac08f60f532c
Иван Зыкин <faustganibal4@gmail.com>
2022-04-04 21:04:53 +0600

    Написана программа для подсчета наполнения бассейна

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int volume = 1200;
+        int fillingSpeed = 30;
+        int devastationSpeed = 10;
+        int actualSpeed = fillingSpeed - devastationSpeed;
+        int count = 0;
+        int fullPool = 0;
+
+        while (true) {
+            if (fullPool < volume){
+                fullPool += actualSpeed;
+                count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println("Бассейн наполнится за " + count + " минут");
     }
 }
